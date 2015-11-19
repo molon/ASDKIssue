0c3907356cb49e6628cbd6dbacbe5f6ac617e564
molon <dudl@qq.com>
2015-11-19 16:33:32 +0800

    Initial Commit

---
+++ ASDKIssue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ASDKIssue
+//
+//  Created by molon on 15/11/19.
+//  Copyright © 2015年 molon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ASDKIssue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ASDKIssue
+//
+//  Created by molon on 15/11/19.
+//  Copyright © 2015年 molon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
